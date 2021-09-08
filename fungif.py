15d06b119ae5cee47da97c6c9ca22a0417733b31
hansel <bbhansel@gmail.com>
2021-09-08 06:20:34 -0400

    Update fungif.py

--- fungif.py
+++ fungif.py
@@ -14,37 +14,48 @@ def exp(z):
     return np.exp(z)
 def arg(z):
     return np.angle(z)/(2*m.pi)
+def pow(z,w):
+    return np.power(z,w)
+def oss(x):
+    return np.sin(x*2*m.pi)
+
 pi = m.pi
 
 print()
 filename = input("filename: ")
 ext = '.'+input("filetype: ")
 numframes = int(input("# frames: "))
+sidelen = int(input("sidelen : "))
+print()
+print("f is a real-valued f'n taking cpx input z and time input t 0->1")
+print(" cpx valued f'ns: sin cos tan pow exp")
+print("real valued f'ns: abs arg .real .imag")
 print()
 func = input("f(z,t)=")
 print()
 
-numcols, numrows = 540,540
-w, h = 5, 5
+numcols, numrows = sidelen,sidelen
+w,h = 5,5
 fps = 30
 
-fig = plt.figure(figsize=(w, h), dpi=1.3*numcols/w,frameon = False)
+fig = plt.figure(figsize=(w,h),dpi=1.3*numcols/w,frameon=False)
 
-x = np.arange(-w/2,w/2,w/numcols)
-y = np.arange(-h/2,h/2,h/numrows)
+xx = np.arange(-w/2,w/2,w/numcols)
+yy = np.arange(-h/2,h/2,h/numrows)
 
-xx, yy = np.meshgrid(x,y)
-z = xx + 1j * yy
+x, y = np.meshgrid(xx,yy)
+z = x + 1j * y
 
 ims = []
 
 for i in range(numframes+1):
     t = i/numframes
-    print("painting frame",i,"of",numframes,end='\r')
+    print("Painting frame",i,"of",numframes,end='\r')
     exec("f="+func)
+
     plt.axis('off')
-    im = plt.pcolormesh(x,y,np.mod(f,1),animated = True,vmin = 0,vmax = 1,
-                        shading = 'auto',cmap = 'hsv')
+    im = plt.pcolormesh(xx,yy,np.mod(f,1),animated = True,vmin = 0,vmax = 1,
+                        shading = 'auto',cmap = 'twilight')
     ims.append([im])
 
 print()
@@ -57,7 +68,7 @@ if not(os.path.isdir('FUN_GIFS')):
 while os.path.exists(direc+filename+ext):
     filename += 'x'
 
-print("saving "+filename+"...")
+print("Saving "+filename+"...")
 
 ani = animation.ArtistAnimation(fig, ims, interval=1000/fps)
 ani.save(filename+ext)
