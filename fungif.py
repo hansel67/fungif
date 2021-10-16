e8d1bdbeb84e26eee6ba7d11cdf3c092349f5918
hansel <bbhansel@gmail.com>
2021-10-16 16:01:47 -0400

    Update fungif.py

--- fungif.py
+++ fungif.py
@@ -1,76 +1,89 @@
-import os, imageio
+import os, sys, imageio
 import numpy as np
-from numpy import cos,sin,tan,exp,pi
-
-def arg(z):
-    return np.angle(z)/(2*m.pi)
-def pow(z,w):
-    return np.power(z,w)
-def re(z):
-    return z.real
-def im(z):
-    return z.imag
-def min(a,b):
-    return np.minimum(a,b)
-def max(a,b):
-    return np.maximum(a,b)
-def bump(x,p):
-    x = np.mod(x,0.99)+0.0001
-    return np.exp((1/(x*(x-1))+4)*p)
+from numpy import cos,sin,tan,exp,pi,floor,ceil,mod
+from numpy import power as pow, minimum as min, maximum as max
+from funcs import *
 
 i = 1j
-myfps = 24
-scale = 2
-h = w = 540
-numbeats = 4
-bpm = 60
-filename = 'fungif'
-funstr =">\"F*.U/'N-_:_-G'\I.*F\"<"
-numframes = int(numbeats*myfps*60/bpm)
-dt = numframes/30
-
-os.system("cls")
-print()
-print(funstr)
-print()
-func =  input("f(z,t) = ")
-print()
-
-dz = scale*2/min(h,w)
-yy = np.arange(-scale,scale,dz)
-xx = np.arange(-scale,scale,dz)
-x, y = np.meshgrid(xx,yy)
-f = z = x + 1j * y
+funstr = ">\"F*.U/'N-_:_-G'\I.*F\"<"
 
 cwd = os.getcwd()
 direc = cwd+'\\fungifs\\'
+
 if not(os.path.isdir('fungifs')):
     os.system("mkdir fungifs")
 
-while os.path.exists(direc+filename+'.gif'):
-    if filename == 'fungif':
-        filename = 'fungif1'
+if not(os.path.exists('fungifs\\fungif_settings.txt')):
+    settings_file = open(direc+'fungif_settings.txt',"w")
+    settings_file.write("""filename = 'fungif'
+h = 540
+w = 540
+fps = 24.0
+numbeats = 4.0
+bpm = 60.0
+scale = 1.0""")
+    settings_file.close()
+
+while(True):
+
+    os.system("cls")
+    print()
+    print(funstr)
+    print()
+    func = input("(r,g,b)(z,t) = ")
+    print()
+
+    settings_file = open(direc+'fungif_settings.txt',"r")
+    settings = settings_file.read()
+    exec(settings)
+    settings_file.close()
+
+    if p != "":
+        if os.path.exists(p):
+            inpt = Image.open(p)
+            iw, ih = inpt.size
+            p = asarray(inpt)
+            inpt.close()
+
+    numframes = int(numbeats*fps*60/bpm)
+    dt = numframes/30
+    dz = 2*scale/min(h,w)
+    yy = np.arange(-(h/2)*dz,(h/2)*dz,dz)
+    xx = np.arange(-(w/2)*dz,(w/2)*dz,dz)
+    x, y = np.meshgrid(xx,yy)
+    z = x + 1j * y
+
+    if func == '':
+        sys.exit()
+    if func == '?':
+        os.startfile(direc + "fungif_settings.txt")
         continue
-    else:
-        filename = filename[0:6]+str(int(filename[6:len(filename)+1])+1)
 
-with imageio.get_writer(direc+filename+'.gif', mode='I',fps = myfps) as writer:
-    for k in range(numframes):
-        t = k/numframes
-        print(funstr[0:int(t*len(funstr))+1],end='\r')
-        exec("f ="+func)
-        r = 1-bump(f,1.3)
-        g = 1-bump(f+2/3,1.3)
-        b = 1-bump(f+1/3,1.3)
-        data = np.dstack((r,g,b))*255
-        image = data.astype(np.uint8)
-        writer.append_data(image)
+    initname = filename
+    initlen = len(initname)
 
-descrip = open(direc+filename+'.txt',"w")
-descrip.write(filename+"="+func)
-descrip.close()
+    while os.path.exists(direc+filename+'.gif'):
+        if filename == initname:
+            filename = initname+'1'
+            continue
+        else:
+            filename = str(filename[0:initlen])+str(int(filename[initlen:len(filename)+1])+1)
 
-print()
-print()
+    with imageio.get_writer(direc+filename+'.gif', mode='I',fps = fps) as writer:
+        for k in range(numframes):
+            t = k/numframes
+            print(funstr[0:int(t*len(funstr))+1],end='\r')
+            exec("F ="+func)
+            data = np.dstack(F)*255
+            image = data.astype(np.uint8)
+            writer.append_data(image)
 
-os.system(direc+filename+'.gif')
+    descrip = open(direc+filename+'.txt',"w")
+    descrip.write(filename+" = "+func+"\n\n")
+    descrip.write(settings)
+    descrip.close()
+
+    print()
+    print()
+
+    os.system(direc+filename+'.gif')
